6767709e5a3587e17ec12852a67eebe04c8af293
OFLASH\thema <marcelovargas.bo@gmail.com>
2025-10-09 19:22:25 -0300

    Migrations

---
+++ Moto/MotoApi/Migrations/20251009222042_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MotoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Motos",
+                columns: table => new
+                {
+                    Identificador = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ano = table.Column<int>(type: "integer", nullable: false),
+                    Modelo = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Placa = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motos", x => x.Identificador);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Motos_Placa",
+                table: "Motos",
+                column: "Placa",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Motos");
+        }
+    }
+}
