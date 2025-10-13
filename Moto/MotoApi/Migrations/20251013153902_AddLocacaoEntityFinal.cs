848757723aa6af6494c931714c3a5f9d3d10f498
OFLASH\thema <marcelovargas.bo@gmail.com>
2025-10-13 13:20:21 -0300

    Remove Locacao entity definition and migration files

---
+++ Moto/MotoApi/Migrations/20251013153902_AddLocacaoEntityFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MotoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocacaoEntityFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
